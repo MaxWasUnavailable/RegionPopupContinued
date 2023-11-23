63efa44a7503b3fe68f2942f7fb6fe1dc7cc0cc2
Max <89798523+MaxWasUnavailable@users.noreply.github.com>
2023-11-23 03:41:16 +0100

    Add patching logic

--- RegionPopupContinued/RegionPopupContinued.cs
+++ RegionPopupContinued/RegionPopupContinued.cs
@@ -1,13 +1,39 @@
 ﻿using BepInEx;
+using BepInEx.Logging;
+using HarmonyLib;
 
 namespace RegionPopupContinued;
 
 [BepInPlugin(PluginInfo.PLUGIN_GUID, PluginInfo.PLUGIN_NAME, PluginInfo.PLUGIN_VERSION)]
 public class RegionPopupContinued : BaseUnityPlugin
 {
+    private new static readonly ManualLogSource Logger = new(PluginInfo.PLUGIN_NAME);
+    internal static RegionPopupContinued Instance { get; private set; }
+    private static Harmony Harmony { get; set; }
+    private static bool IsPatched { get; set; }
+
     private void Awake()
     {
-        // Plugin startup logic
+        Instance = this;
+
+        Harmony = new Harmony(PluginInfo.PLUGIN_GUID);
+
+        PatchAll();
+
         Logger.LogInfo($"Plugin {PluginInfo.PLUGIN_GUID} is loaded!");
+    }
+
+    public void PatchAll()
+    {
+        if (IsPatched) return;
+        Harmony.PatchAll();
+        IsPatched = true;
+    }
+
+    public void UnpatchAll()
+    {
+        if (!IsPatched) return;
+        Harmony.UnpatchAll();
+        IsPatched = false;
     }
 }
